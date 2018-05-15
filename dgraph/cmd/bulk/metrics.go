673ea84580b1b131adbb8c8264300cf412836f21
Rifki <rifky_@outlook.com>
2018-05-15 12:09:52 +0700

    change metric name from dash to underscore due to incompability on prometheus 2.x

--- dgraph/cmd/bulk/metrics.go
+++ dgraph/cmd/bulk/metrics.go
@@ -10,7 +10,7 @@ package bulk
 import "expvar"
 
 var (
-	NumBadgerWrites     = expvar.NewInt("dgraph-bulk-loader_badger_writes_pending")
-	NumReducers         = expvar.NewInt("dgraph-bulk-loader_num_reducers_total")
-	NumQueuedReduceJobs = expvar.NewInt("dgraph-bulk-loader_reduce_queue_size")
+	NumBadgerWrites     = expvar.NewInt("dgraph_bulk_loader_badger_writes_pending")
+	NumReducers         = expvar.NewInt("dgraph_bulk_loader_num_reducers_total")
+	NumQueuedReduceJobs = expvar.NewInt("dgraph_bulk_loader_reduce_queue_size")
 )
